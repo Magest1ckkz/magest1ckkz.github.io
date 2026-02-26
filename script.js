230741fa646002f7ff8c15234136053c979457ea
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2026-02-26 16:36:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function copyText(){
-    navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="96px" alt="mage_webring">\n</a>`)
+    navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="104px" height="32px" alt="mage_webring">\n</a>`)
 
     }
 
@@ -10,3 +10,4 @@ let isMobileDevice = regexp.test(details);
 if (isMobileDevice) {
     alert("This page is better viewed with a computer screen. If you believe you got here by mistake, click back.")
 }
+
