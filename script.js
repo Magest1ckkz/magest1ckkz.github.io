f37d956c16ce9c38c2ba21e0b17c246d6b5e72f8
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2026-02-26 09:48:04 +0300

    Website redesign

--- script.js
+++
@@ -1,3 +0,0 @@
-function copyText(){
-navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/magestick.png">\n</a>`)
-}
