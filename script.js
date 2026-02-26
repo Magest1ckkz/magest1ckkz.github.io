f0357e47f954360224f0f8d1573ba8980c822e82
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2026-02-26 17:59:30 +0300

    Remove mobile warning

--- script.js
+++ script.js
@@ -2,12 +2,3 @@ function copyText(){
     navigator.clipboard.writeText(`<a href="https://magest1ckkz.github.io/">\n<img src="https://raw.githubusercontent.com/Magest1ckkz/magest1ckkz.github.io/refs/heads/main/mage-webring.png" width="104px" height="32px" alt="mage_webring">\n</a>`)
 
     }
-
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i;
-let isMobileDevice = regexp.test(details);
-
-if (isMobileDevice) {
-    alert("This page is better viewed with a computer screen. If you believe you got here by mistake, click back.")
-}
-
